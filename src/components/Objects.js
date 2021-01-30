a31f4031b76eb3e21273a05ada915e8ea01f8861
Piotr Sadowski <piotrsadowski09@gmail.com>
2021-01-30 21:19:59 +0100

    Data

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -4,46 +4,21 @@ import React, { Component } from 'react'
     render() {
         const objectName = this.props.list.map((e)=>{
             let nearOb = Object.values(e.near_earth_objects).find(a => a[0]);
-            const flattenData = [].concat.apply([], nearOb);
-            const wantedResultData = flattenData.map(el => <a href={el.nasa_jpl_url} rel='noreferrer' target='_blank' key={el.id}>{el.name}</a>);
-            return wantedResultData
+            const flattenDataName = [].concat.apply([], nearOb);
+            const wantedName = flattenDataName.map(el => 
+                <li key={el.id}> <a href={el.nasa_jpl_url} rel='noreferrer' target='_blank' >
+                        {el.name}</a> Czy pierdolnie? --- <span>{el.is_potentially_hazardous_asteroid ===true? "Yes":"No"}</span>
+                        <span>  odległość  ---{Math.round(el.close_approach_data[0].miss_distance.kilometers)}kilometrów</span>
+                        <span>  rozmiar ---{Math.round(el.estimated_diameter.meters.estimated_diameter_max)}metrów</span>
+                </li>);
+            return wantedName
         });
-        const objectDistance = this.props.list.map((e) =>{
-            let nearOb = Object.values(e.near_earth_objects).find(a => a[0]).map(b => b.close_approach_data);
-            const flattenData = [].concat.apply([],nearOb);
-            const wantedResultData = flattenData.map(obj => obj.miss_distance);
-            const flattenDataB = [].concat.apply([], wantedResultData);
-            const wantedResultDataB = flattenDataB.map(obj => <p key={obj.kilometers}>{Math.round(obj.astronomical *100)/100} </p>);
-            return wantedResultDataB
-        });
-        const objectSize = this.props.list.map((e)=> {
-            let nearOb = Object.values(e.near_earth_objects).find(a => a[0]).map(b => b.estimated_diameter);
-            const flattenData =  [].concat.apply([], nearOb);
-            const wantedResultData = flattenData.map(obj => obj.meters);
-            const flattenDataB =  [].concat.apply([],wantedResultData );
-            const wantedResultDataB = flattenDataB.map(obj => <p  key={obj.estimated_diameter_min} > {Math.round(obj.estimated_diameter_max)}</p>);
-            return wantedResultDataB
-        });
-
-        const objectDanger = this.props.list.map((e)=> {
-            let nearOb = Object.values(e.near_earth_objects).find(a => a[0])
-            const flattenData =  [].concat.apply([], nearOb);
-            console.log(flattenData)
-            const wantedResultData = flattenData.map(obj => <a key={obj.name} href="https://en.wikipedia.org/wiki/Potentially_hazardous_object" target="_blank" rel='noreferrer'> {obj.is_potentially_hazardous_asteroid ===true? "Yes":"No"} </a>);
-            return wantedResultData
-
-        });
-
-
-
-
+   
 
         return (
             <ul>
-                <li>{objectName}</li>
-                <li>{objectDistance}</li>
-                <li>{objectSize}</li>
-                <li>{objectDanger}</li>
+                {objectName}
+               
             </ul>
         )
     }
